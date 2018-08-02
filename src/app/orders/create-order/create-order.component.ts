58b61cb09a7ae03b722e1e19ea4baa580956ea22
Analia Fatta <afatta@lendingclub.com>
2018-08-02 07:28:12 -0300

    Adding create order

--- src/app/orders/create-order/create-order.component.ts
+++ src/app/orders/create-order/create-order.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { DataService } from '../../services/data.service';
-import { Cliente } from '../../models/models';
+import { OrdersService } from '../../services/orders.service'
+import { Cliente, Address, Flete, Art, DetalleArticulo, Variante, Peditem } from '../../models/models';
 
 
 @Component({
@@ -10,29 +11,63 @@ import { Cliente } from '../../models/models';
   styleUrls: ['./create-order.component.css']
 })
 export class CreateOrderComponent implements OnInit {
-  sellerId : string;
-  clients : Cliente[];
-  selectedClient : string;
-  client: Cliente;
-
-  constructor( private dataservice : DataService, private userService : UserService) { 
+  sellerId: string;
+  clients: Cliente[];
+  clientId: string;
+  selectedClient: Cliente;
+  addressSelectedId: string;
+  selectedAddress: Address;
+  selectedFlete: Flete;
+  articulos: Art[];
+  artId: number;
+  articulo: DetalleArticulo;
+  hasVariantes: boolean;
+  variantes: Variante[];
+  //expreso : Expreso [];
+  selectedItems: Peditem[];
+  constructor(private dataservice: DataService, private userService: UserService, private orderService: OrdersService) {
 
   }
 
   ngOnInit() {
     this.sellerId = this.dataservice.getSellerId();
+    this.userService.getClientsBySeller(this.sellerId).subscribe((data: Cliente[]) => {
+      this.clients = data;
+      this.selectedClient = this.clients[0];
+    });
+    // LOADING ARTICULOS
+    this.orderService.getArticulos().subscribe((data: Art[]) => {
+      this.articulos = data;
+    });
 
-    this.userService.getClientsBySeller(this.sellerId).subscribe((data : Cliente[] ) => {
-    this.clients = data;
-    console.log("call clients works... "+ this.clients)
+
+  }
+
+  onClientSelected(event: any) {
+    console.log('Selected value');
+    console.log(event);
+    this.clientId = event;
+    this.userService.getClient(this.clientId).subscribe((data: Cliente) => {
+      this.selectedClient = data;
+      this.selectedAddress = this.selectedClient.address[0];
+      this.selectedFlete = this.selectedAddress.flete;
+
+      console.log("call client works... " + this.clientId)
     });
   }
 
-  onClientSelected(){
-   this.userService.getClient(this.selectedClient).subscribe((data :Cliente) => {
-   this.client = data;
-   console.log("call client works... "+ this.selectedClient)
+  onArtSelected(event: any) {
+    this.artId = event;
+    this.orderService.getArticuloById(this.artId).subscribe((data: DetalleArticulo) => {
+      this.articulo = data;      
+      if (this.articulo && this.articulo.variantes && this.articulo.variantes.length > 0) {
+        this.variantes = this.articulo.variantes;
+        this.hasVariantes = true;
+      } else {
+        this.hasVariantes = false;
+      }
+      console.log("call getArticuloById works... " + this.articulo.art_id + " " +   this.variantes.length)
     });
-   }
+  }
 
 }
--- src/app/services/orders.service.ts
+++ src/app/services/orders.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { OrderDetail, Order } from '../models/models';
+import { OrderDetail, Order, Art , DetalleArticulo} from '../models/models';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
@@ -12,6 +12,8 @@ export class OrdersService {
   ROOT_URL = 'http://lumasoft.dyndns.org.:8000/api';
   GET_ORDERS_URL = '/pedcab/vendedor/';
   GET_ORDER_URL = '/pedcab/id/';
+  GET_ARTICULOS_URL = '/articulos/';
+  GET_ART_URL = '/articulos/id/';
 
   constructor(private httpClient : HttpClient) { }
 
@@ -22,6 +24,14 @@ getOrders(sellereId: string) :  Observable<Order[]>{
 
 getOrder(id:string) : Observable<OrderDetail> {
   return this.httpClient.get<OrderDetail>(this.ROOT_URL + this.GET_ORDER_URL + + id)
+}
+
+getArticulos() : Observable<Art[]> {
+  return this.httpClient.get<Art[]>(this.ROOT_URL + this.GET_ARTICULOS_URL)
+}
+
+getArticuloById(id: number) : Observable<DetalleArticulo> {
+  return this.httpClient.get<DetalleArticulo>(this.ROOT_URL + this.GET_ART_URL + id)
 }
 
 }
